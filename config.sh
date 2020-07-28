2ebaec85c110bde83ea67b9ee122491a85bff78a
yuta-natsume <natsume.yuta@classmethod.jp>
2020-07-29 00:56:24 +0900

    update package.json

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat <<EOS > src/aws-config.ts
+cat <<EOT > src/aws-config.ts
 export const DATA_BUCKET_NAME = "${DATA_BUCKET_NAME}";
 export const IDENTITY_POOL_ID = "${IDENTITY_POOL_ID}";
 export const AWS_REGION = "us-east-1";
